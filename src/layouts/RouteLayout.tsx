c85a99b6710849324da34c8b28c73cc8a7088e84
Luis Caballero <luisc@192.168.0.5>
2025-10-30 17:51:24 -0500

    initial login with email and password, instaltion of shaddcn

--- src/layouts/RouteLayout.tsx
+++ src/layouts/RouteLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router"
+import { Toaster } from "sonner"
 
 const RouteLayout = () => {
   return (
     <div>
       <Outlet />
+      <Toaster className="top-right" richColors/>
     </div>
   )
 }
--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,8 +1,34 @@
+import { Button } from "@/components/ui/button"
+import { useAuthActions } from "../../hooks/useAuthActions"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+
 const LoginPage = () => {
+
+  const { loginWithGoogle} = useAuthActions()
+  
+  const handleLoginWithGoogle = async () => {
+    const response = await loginWithGoogle()
+    if (!response.success) {
+      console.error(response.error)
+      return
+    }
+    console.log(response)
+  }
   return (
-    <div>
-      Login
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>
+          Login to your acoutn using email and password
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+
+      </CardContent>
+      <CardFooter>
+        <Button onClick={handleLoginWithGoogle} className="w-full">Login with Google</Button>
+      </CardFooter>
+    </Card>
   )
 }
 
